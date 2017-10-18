83f303e6a7b5749b635831bac395b4080f1da071
Unknown <megaman97ESP@gmail.com>
2017-10-18 10:09:14 +0300

    First commit, prototype
    All placeholder, very basic movement and limited functionality

---
+++ Assets/CharacterControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+	public float speed;
+	public float jumpSpeed;
+
+
+	void Update(){
+		Transform playerTransform = this.gameObject.transform;
+		playerTransform.rotation = new Quaternion (0, 0, 0, 0);
+		float moveVertical = 0;
+		if( Input.GetKeyDown(KeyCode.Space))
+			moveVertical=jumpSpeed;
+		Rigidbody2D playerRigidBody=this.gameObject.GetComponent<Rigidbody2D>();
+		Vector2 movement = new Vector2 (playerRigidBody.velocity.x, playerRigidBody.velocity.y+moveVertical);
+		playerRigidBody.velocity = movement;
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Rigidbody2D playerRigidBody=this.gameObject.GetComponent<Rigidbody2D>();
+		Vector2 movement = new Vector2 (moveHorizontal * speed, playerRigidBody.velocity.y);
+		playerRigidBody.velocity = movement;
+	}
+}

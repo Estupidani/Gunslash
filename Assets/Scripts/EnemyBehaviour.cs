4d18c8be7abe908265563bc9e5915938ddf19f75
Unknown <megaman97ESP@gmail.com>
2017-10-23 17:01:25 +0300

    Updated enemy AI
    If the enemy gets close it starts attacking. For now it only stops, but the template can be used to implement an animation and an attack.

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,22 +4,30 @@ using System.Collections;
 public class EnemyBehaviour : MonoBehaviour {
 	public float speed;
 	public float jumpSpeed;
+	public Boundary boundary;
+	public float attackLength;
 
 	private Rigidbody2D enemyRigidBody;
 	private float moveHorizontal;
 	private float moveVertical;
+	private bool isAttacking;
+	private float timeAttack;
 
 	void Start () {
+		isAttacking = false;
 		enemyRigidBody = this.gameObject.GetComponent<Rigidbody2D> ();
 		moveHorizontal = -1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (enemyRigidBody.IsSleeping ()) {
+		if (enemyRigidBody.IsSleeping () && !isAttacking) {
 			moveHorizontal = -moveHorizontal;
 			moveVertical = jumpSpeed;
 		}
+		CheckBoundaries ();
+		if (isAttacking && Time.time > timeAttack + attackLength)
+			isAttacking = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
@@ -30,10 +38,29 @@ public class EnemyBehaviour : MonoBehaviour {
 		if(other.CompareTag("JumpBoundaryFromRight") && enemyRigidBody.velocity.x < 0f )
 			moveVertical = jumpSpeed;
 	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.CompareTag ("PlayerInteractHitbox"))
+			Attack ();
+	}
 		
 	void FixedUpdate(){
 		Vector2 movement = new Vector2 (moveHorizontal * speed, enemyRigidBody.velocity.y+moveVertical);
 		moveVertical = 0f;
-		enemyRigidBody.velocity = movement;
+		if(!isAttacking)
+			enemyRigidBody.velocity = movement;
+	}
+
+	void Attack(){
+		timeAttack = Time.time;
+		Vector2 auxMovement = new Vector2 (moveHorizontal, moveVertical);
+		isAttacking = true;
+		enemyRigidBody.velocity = new  Vector2 (0f, 0f);
+	}
+
+	void CheckBoundaries(){ //Checks wether the character is out of bounds. If it is, prevents it from going further
+		enemyRigidBody.position = new Vector2 (
+			Mathf.Clamp (enemyRigidBody.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp (enemyRigidBody.position.y, boundary.yMin, boundary.yMax));
 	}
 }
